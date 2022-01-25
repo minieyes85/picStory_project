ffc6917c39b6ddaffc01af55a1c11009ea6ba882
JSChoi <minieyes85@gmail.com>
2022-01-25 21:47:58 +0900

    220125 환경설정 업데이트, database config

---
+++ src/main/java/com/minieyes/picstory/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package com.minieyes.picstory.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+@MapperScan(basePackages="com.minieyes.picstory.*")  // interface 파일 package 경로
+public class DatabaseConfig {
+	
+	@Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+
+        return sessionFactory.getObject();
+    }
+
+}
