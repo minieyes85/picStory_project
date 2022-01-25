8d2fd990ce32e94708cc48064c4b7aa626b65d12
JSChoi <minieyes85@gmail.com>
2022-01-25 21:33:54 +0900

    220125 picstory first commit

---
+++ src/main/java/com/minieyes/picstory/PicStoryApplication.java
@@ -0,0 +1,13 @@
+package com.minieyes.picstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicStoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minieyes/picstory/PicStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minieyes.picstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
