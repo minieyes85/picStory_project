121ede016ab5df065d6badf65b2fb6b6e9ae55dd
JS Choi <minieyes85@gmail.com>
2022-01-26 01:00:11 +0900

    220126 file upload 구현 테스트

--- src/main/java/com/minieyes/picstory/TestController.java
+++ src/main/java/com/minieyes/picstory/TestController.java
@@ -3,6 +3,7 @@ package com.minieyes.picstory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class TestController {
@@ -10,6 +11,7 @@ public class TestController {
 	@GetMapping("/test")
 	@ResponseBody
 	public String test() {
+		
 		return "Hello World";
 	}
 }
