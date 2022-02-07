cac80637477f808a7b5cb6ac43ff6fce14cc6eec
JSChoi <minieyes85@gmail.com>
2022-02-07 21:49:41 +0900

    220207 test warning 처리

--- src/main/java/com/minieyes/picstory/TestController.java
+++ src/main/java/com/minieyes/picstory/TestController.java
@@ -3,7 +3,6 @@ package com.minieyes.picstory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class TestController {
