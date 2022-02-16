750029f8a07425fe33d8872c72ba91c7a174df10
JS Choi <minieyes85@gmail.com>
2022-02-16 10:55:16 +0900

    220216 아이콘 수정 완료

--- src/main/java/com/minieyes/picstory/post/PostController.java
+++ src/main/java/com/minieyes/picstory/post/PostController.java
@@ -1,7 +1,6 @@
 package com.minieyes.picstory.post;
 
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.minieyes.picstory.comment.model.Comment;
 import com.minieyes.picstory.post.bo.PostBO;
 import com.minieyes.picstory.post.model.Post;
 import com.minieyes.picstory.post.model.PostDetail;
